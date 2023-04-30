4fab05caddae1986644aa6454cac052ab0e13809
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-04-30 03:40:32 +0300

    refactor useaccount class

--- src/UseAccount.java
+++ src/UseAccount.java
@@ -1,5 +1,4 @@
 import static java.lang.System.out;
-
 public class UseAccount {
     public static void main(String[] args) {
         Account myAccount = new Account();
@@ -11,7 +10,14 @@ public class UseAccount {
         yourAccount.address = "121 Outer Street";
         yourAccount.balance = 55.63;
         myAccount.display();
-        System.out.println();
+        out.print(" плюс $");
+        out.print(myAccount.getInterest(5.00));
+        out.println(" дохода ");
         yourAccount.display();
+        double yourInterestRate = 7.00;
+        out.print(" плюс $");
+        double yourInterestAmout = yourAccount.getInterest(yourInterestRate);
+        out.print(yourInterestAmout);
+        out.println(" дохода ");
     }
 }
