5d680e3ccc7c8b0c018237c5b9540c249c235639
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-04-30 03:37:45 +0300

    add useaccount class

---
+++ src/UseAccount.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+
+public class UseAccount {
+    public static void main(String[] args) {
+        Account myAccount = new Account();
+        Account yourAccount = new Account();
+        myAccount.name = "Барри";
+        myAccount.address = "222 Inner Lane";
+        myAccount.balance = 22.02;
+        yourAccount.name = "Джейн";
+        yourAccount.address = "121 Outer Street";
+        yourAccount.balance = 55.63;
+        myAccount.display();
+        System.out.println();
+        yourAccount.display();
+    }
+}
